cbef1d638a456ae0c2420e58bae63fe1d4616d81
GFRrank <146967947+GFRrank@users.noreply.github.com>
2023-11-22 20:24:38 -0300

    error

--- lib/view/Cadastro/CadastroApp.dart
+++ lib/view/Cadastro/CadastroApp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../../controller/login_controller.dart';
 import '../../main.dart';
 
@@ -19,8 +20,13 @@ class _CadastroAppState extends State<CadastroApp> {
   var txtSetor = TextEditingController();
   var txtLoja = TextEditingController();
 
+<<<<<<< HEAD
   List<String> cargos = ['Funcionário Comum', 'Gerente'];
   String? cargoSelecionado = 'Funcionário Comum';
+=======
+  List<String> Cargos = ['Funcionário Comum', 'Gerente'];
+  String? CargoSelecionado = 'Funcionário Comum';
+>>>>>>> c29ee9ceed72ee8841946b72de4aac400301e040
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +38,7 @@ class _CadastroAppState extends State<CadastroApp> {
       ),
       body: Column(
         children: [
+<<<<<<< HEAD
           _buildTextField('Nome', txtNome),
           _buildTextField('Telefone', txtPhone),
           _buildTextField('Email', txtEmail),
@@ -39,10 +46,100 @@ class _CadastroAppState extends State<CadastroApp> {
           _buildTextField('ID', txtID, obscureText: true),
           _buildDropdownButton(),
           _buildElevatedButton(),
+=======
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+            child: TextField(
+              controller: txtNome,
+              decoration: InputDecoration(labelText: 'Nome'),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+            child: TextField(
+              controller: txtPhone,
+              inputFormatters: [LengthLimitingTextInputFormatter(11)],
+              decoration: InputDecoration(labelText: 'Telefone'),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+            child: TextField(
+              controller: txtEmail,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+            child: TextField(
+              controller: txtSenha,
+              decoration: InputDecoration(
+                labelText: 'Senha',
+              ),
+              obscureText: true,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+            child: TextField(
+              controller: txtID,
+              inputFormatters: [LengthLimitingTextInputFormatter(10)],
+              decoration: InputDecoration(
+                labelText: 'ID',
+              ),
+              obscureText: false,
+            ),
+          ),
+          SizedBox(height: 20),
+          SizedBox(
+            width: 200,
+            height: 60,
+            child: DropdownButtonFormField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(32),
+                  borderSide: BorderSide(color: Colors.green),
+                ),
+              ),
+              value: CargoSelecionado,
+              items: Cargos.map((cargo) => DropdownMenuItem(
+                    value: cargo,
+                    child: Text(cargo),
+                  )).toList(),
+              onChanged: (cargo) => setState(() => CargoSelecionado = cargo),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+            child: ElevatedButton(
+              onPressed: () {
+                LoginController().criarConta(
+                  context,
+                  txtNome.text,
+                  txtEmail.text,
+                  txtSenha.text,
+                  txtID.text,
+                  txtPhone.text,
+                  txtLoja.text,
+                  txtCargo.text,
+                  txtSetor.text,
+                );
+                Navigator.pushReplacementNamed(context, 'Login');
+              },
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green[800],
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30))),
+              child: const Text('Cadastrar',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+            ),
+          ),
+>>>>>>> c29ee9ceed72ee8841946b72de4aac400301e040
         ],
       ),
     );
   }
+<<<<<<< HEAD
 
   Widget _buildTextField(String labelText, TextEditingController controller, {bool obscureText = false}) {
     return Padding(
@@ -111,4 +208,6 @@ class _CadastroAppState extends State<CadastroApp> {
       txtSenha.text,
     );
   }
+=======
+>>>>>>> c29ee9ceed72ee8841946b72de4aac400301e040
 }
